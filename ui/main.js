66d8378fdb1c02f491b8ba87c3cffdf01b77fd9d
HariPrasad005 <hari.8324@gmail.com>
2017-09-04 13:50:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 var img=document.getElementById("madi");
-var marginleft=0;
-function moveright(){
-    marginleft=marginleft+1;
-    img.style.marginleft=marginleft+'px';
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
-    var interval=setInterval(moveright,50);
+    var interval=setInterval(moveRight,50);
     
 };
