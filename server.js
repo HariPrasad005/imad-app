dbb18ab8ee2f90fb9a3616711dab380324f14358
HariPrasad005 <hari.8324@gmail.com>
2017-09-04 14:54:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(countere.toString());
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
